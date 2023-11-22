1eb2fc50bb9d73bb99edefb1827aded175b35a15
zerenluo <luozeren2@gmail.com>
2023-11-22 20:35:40 +0800

    extract biped data from ai4animation

---
+++ datasets/retarget_config_go1_biped.py
@@ -0,0 +1,69 @@
+import numpy as np
+from legged_gym import LEGGED_GYM_ROOT_DIR
+
+
+VISUALIZE_RETARGETING = True
+
+URDF_FILENAME = "{LEGGED_GYM_ROOT_DIR}/resources/robots/go1_description/urdf/go1_re.urdf".format(
+    LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR)
+
+OUTPUT_DIR = "{LEGGED_GYM_ROOT_DIR}/datasets/mocap_motions_go1_biped".format(
+    LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR)
+
+REF_POS_SCALE = 0.825
+INIT_POS = np.array([0, 0, 0.32])
+INIT_ROT = np.array([0, 0, 0, 1.0])
+
+SIM_TOE_JOINT_IDS = [6, 11, 16, 21]
+SIM_HIP_JOINT_IDS = [2, 7, 12, 17]
+SIM_ROOT_OFFSET = np.array([0, 0, -0.04])
+SIM_TOE_OFFSET_LOCAL = [
+    np.array([0, 0.06, 0.0]),
+    np.array([0, -0.06, 0.0]),
+    np.array([0, 0.06, 0.0]),
+    np.array([0, -0.06, 0.0])
+]
+TOE_HEIGHT_OFFSET = 0.02
+
+DEFAULT_JOINT_POSE = np.array([0, 0.9, -1.8, 0, 0.9, -1.8, 0, 0.9, -1.8, 0, 0.9, -1.8])
+JOINT_DAMPING = [0.1, 0.05, 0.01,
+                 0.1, 0.05, 0.01,
+                 0.1, 0.05, 0.01,
+                 0.1, 0.05, 0.01]
+
+FORWARD_DIR_OFFSET = np.array([0, 0, 0])
+
+FR_FOOT_NAME = "FR_foot"
+FL_FOOT_NAME = "FL_foot"
+HR_FOOT_NAME = "RR_foot"
+HL_FOOT_NAME = "RL_foot"
+
+MOCAP_MOTIONS = [
+    # Output motion name, input file, frame start, frame end, motion weight.
+    [
+        "biped_0",
+        "{LEGGED_GYM_ROOT_DIR}/datasets/keypoint_datasets/ai4animation/dog_beg01_joint_pos.txt".format(
+            LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR), 2740, 2845, 1.0
+    ],
+    [
+        "biped_1",
+        "{LEGGED_GYM_ROOT_DIR}/datasets/keypoint_datasets/ai4animation/dog_beg00_joint_pos.txt".format(
+            LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR), 1000, 1200, 1.0
+    ],
+    [
+        "biped_2",
+        "{LEGGED_GYM_ROOT_DIR}/datasets/keypoint_datasets/ai4animation/dog_beg00_joint_pos.txt".format(
+            LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR), 1280, 1460, 2.0
+    ],
+    [
+        "biped_3",
+        "{LEGGED_GYM_ROOT_DIR}/datasets/keypoint_datasets/ai4animation/dog_beg00_joint_pos.txt".format(
+            LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR), 1525, 1725, 1.0
+    ],
+    [
+        "biped_4",
+        "{LEGGED_GYM_ROOT_DIR}/datasets/keypoint_datasets/ai4animation/dog_beg00_joint_pos.txt".format(
+            LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR), 2670, 2733, 1.5
+    ],
+
+]
--- datasets/retarget_kp_motions.py
+++ datasets/retarget_kp_motions.py
@@ -18,7 +18,8 @@ import pybullet_data as pd
 
 from datasets.retarget_utils import *
 # from datasets import retarget_config_a1 as config
-from datasets import retarget_config_go1 as config
+# from datasets import retarget_config_go1 as config
+from datasets import retarget_config_go1_biped as config
 
 POS_SIZE = 3
 ROT_SIZE = 4
